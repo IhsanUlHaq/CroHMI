f1bf72c22224ea3408fdffef4d8b472393ee8a3c
Moeez Malik <moeezmalik13@gmail.com>
2019-02-07 15:17:46 +0500

    Auto Power On for GSM

--- Misc/TinyGSMExample/TinyGSMExample.ino
+++ Misc/TinyGSMExample/TinyGSMExample.ino
@@ -53,7 +53,7 @@ const char pass[] = "";
 
 // Server details
 const char server[] = "111.68.101.20";
-const char resource[] = "/CropHealth/datauploadscript.php?dat=2019-02-06+11:22:34&airm=104&airt=101&soilm=102&soilt=103&lat=12.11&lng=13.11&pn=1";
+const char resource[] = "/CropHealth/datauploadscript.php?dat=2019&lat=12.11&lng=11.11&airm1=13&airt1=13&soilm1=13&soilt1=13&pn1=1&airm2=13&airt2=13&soilm2=13&soilt2=13&pn2=2&airm3=13&airt3=13&soilm3=13&soilt3=13&pn3=3&airm4=13&airt4=13&soilm4=13&soilt4=13&pn4=4&airm5=13&airt5=13&soilm5=13&soilt5=13&pn5=5";
 const int  port = 80;
 
 #ifdef DUMP_AT_COMMANDS
@@ -67,14 +67,25 @@ const int  port = 80;
 TinyGsmClient client(modem);
 HttpClient http(client, server, port);
 
+int pwrKey = 9;
+
 void setup() {
   // Set console baud rate
   SerialMon.begin(115200);
   delay(10);
 
+  pinMode(pwrKey, OUTPUT);
+
+  delay(1000);
+  digitalWrite(pwrKey, HIGH);
+  delay(2500);
+  digitalWrite(pwrKey, LOW);
+  delay(1000);
+  
   // Set GSM module baud rate
   SerialAT.begin(115200);
   delay(3000);
+
 
   // Restart takes quite some time
   // To skip it, call init() instead of restart()
